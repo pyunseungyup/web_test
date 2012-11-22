95ad5081948fa7a7b75447b9a0409ccfea2abe2c
pyunseungyup <zpboy@hanmail.net>
2012-11-22 22:27:46 +0900

    database+sql

---
+++ project/WebContent/bnbun.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE users CREATE DATABASE bnbun DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+
+GRANT ALL ON bnbun.* TO 'bnb'@'localhost' IDENTIFIED BY 'bnbun';
+
+use bnbun;
+
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+	userid VARCHAR(100) NOT NULL UNIQUE,
+	pwd VARCHAR(255) NOT NULL,
+    name VARCHAR(100), 
+	phonenumber VARCHAR(100) UNIQUE
+);
+
